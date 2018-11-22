668979626022e044e877f327a2b3ef870453e28e
Kaspar Matas <kaspar.matas@student.manchester.ac.uk>
2018-11-22 10:28:49 +0000

    First processing element
    Added a simple weight computation cell element with its simulation test file.
    
    Github issue #6

---
+++ VerilogSrc/Simulators/weight_comp_cell_simulator.v
@@ -0,0 +1,110 @@
+`timescale 1ns / 1ps
+
+`define DATA_WIDTH 32
+`define CLOCK 20 
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 19.11.2018 14:41:26
+// Design Name: 
+// Module Name: weight_comp_cell_simulator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module weight_comp_cell_simulator();
+
+    event error;
+    always @ (error) begin
+        $display("ERROR at time %t", $time);
+        #`CLOCK $stop;
+    end
+
+    task check_output(input [`DATA_WIDTH:0] result, input [`DATA_WIDTH:0] golden);
+    begin
+        if (result!==golden) begin
+            $display("Output is %0d which should be %0d instead!", result[`DATA_WIDTH-1:0], golden[`DATA_WIDTH-1:0]);
+            ->error;
+        end
+    end
+    endtask
+
+    reg clk;
+    reg [`DATA_WIDTH-1:0] input_index;
+    reg [`DATA_WIDTH-1:0] input_value;
+    reg [`DATA_WIDTH:0] input_result;
+    reg input_enable;
+    wire [`DATA_WIDTH-1:0] output_index;
+    wire [`DATA_WIDTH-1:0] output_value;
+    wire [`DATA_WIDTH:0] output_result;
+    wire output_enable;  
+
+    weight_comp_cell #(
+        .DATA_WIDTH(`DATA_WIDTH), 
+        .WEIGHT_AMOUNT(2), 
+        .WEIGHTS({32'd4, 32'd1})
+    ) uut (
+        .clk(clk), 
+        .input_index(input_index),
+        .input_value(input_value),
+        .input_result(input_result),
+        .input_enable(input_enable),
+        .output_index(output_index),
+        .output_value(output_value),
+        .output_result(output_result),
+        .output_enable(output_enable)
+    );  
+
+initial begin
+    clk = 1;
+    forever #(`CLOCK/2) clk = !clk;
+end
+
+initial begin
+    input_index = 0;
+    input_value = 0;
+    input_result = 0;
+    input_enable = 0;
+end
+
+initial begin
+    #(`CLOCK*2); 
+    input_enable = 1;
+    #(`CLOCK); 
+    check_output(output_result, {1'b0, 32'd0});
+    input_index = 1;
+    input_value = 3'd2;
+    #(`CLOCK); 
+    check_output(output_result, {1'b1, 32'd8});
+    input_index = 0;   
+    input_value = 3'd3;
+    input_result = {1'b1, 32'd6};
+    #(`CLOCK); 
+    check_output(output_result, {1'b1, 32'd6});
+    input_index = 1;   
+    input_value = 3'd4;
+    input_result = {1'b1, 32'd55};
+    #(`CLOCK); 
+    check_output(output_result, {1'b1, 32'd55});
+    input_enable = 0;
+    input_result = {1'b1, 32'd45};
+    #(`CLOCK); 
+    input_result = {1'b0, 32'd100};
+    check_output(output_result, {1'b1, 32'd45});
+    #(`CLOCK); 
+    check_output(output_result, {1'b1, 32'd19});
+    #(`CLOCK*2);
+    $display("SUCCESSFUL TEST!"); 
+    $stop;
+end
+ 
+endmodule
---
+++ VerilogSrc/Sources/weight_comp_cell.v
@@ -0,0 +1,75 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.11.2018 18:03:33
+// Design Name: 
+// Module Name: weight_comp_cell
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+// NEEDS TO HAVE 0 ENABLE AT THE BEGINNING
+
+module weight_comp_cell #(
+        parameter DATA_WIDTH = 32,
+        parameter WEIGHT_AMOUNT = 4,
+        parameter [32*WEIGHT_AMOUNT-1:0] WEIGHTS = {32'd0, 32'd0, 32'd0, 32'd0}
+    ) (
+        input wire clk,
+        input wire [DATA_WIDTH-1:0] input_index,
+        input wire [DATA_WIDTH-1:0] input_value,
+        input wire [DATA_WIDTH:0] input_result,
+        input wire input_enable,
+        output reg [DATA_WIDTH-1:0] output_index,
+        output reg [DATA_WIDTH-1:0] output_value,
+        output reg [DATA_WIDTH:0] output_result,
+        output reg output_enable
+    );
+    
+reg [DATA_WIDTH-1:0] accumulator;
+reg [DATA_WIDTH:0] result;
+
+initial begin 
+    result = 0; 
+end
+    
+always @ (posedge clk) begin
+    if (input_enable) begin
+        if (input_index == 0) accumulator <= 0 + input_value * WEIGHTS[32*input_index +: 8];
+        else if (input_index == WEIGHT_AMOUNT-1 && input_result[DATA_WIDTH]) 
+            result <= {1'b1, accumulator + input_value * WEIGHTS[32*input_index +: 8]};
+        else accumulator <= accumulator + input_value * WEIGHTS[32*input_index +: 8];
+        
+        output_value <= input_value;
+        output_index <= input_index;
+        output_enable <= input_enable;
+    end
+    else begin
+        output_value <= 0;
+        output_index <= 0;
+        output_enable <= 0;
+    end
+    
+    if (input_result[DATA_WIDTH]) output_result <= input_result;
+    else begin
+        if (result[DATA_WIDTH]) begin 
+            output_result <= result;
+            result[DATA_WIDTH] <= 1'b0;
+        end
+        else if (input_enable && input_index == WEIGHT_AMOUNT-1) 
+            output_result <= {1'b1, accumulator + input_value * WEIGHTS[32*input_index +: 8]};
+        else output_result <= 0; 
+    end
+end
+
+endmodule
